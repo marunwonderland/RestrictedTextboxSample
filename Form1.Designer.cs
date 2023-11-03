6f4567a7dae7b27cbd63f4477c6c3c07b10f7198
Tomoyuki <tomo@marunwonderland.com>
2023-11-03 17:09:21 +0900

    Change fontsize for recording

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,11 +53,12 @@
             // 
             this.restrictedTextboxPt.CapableRegexPattern = ".*";
             this.restrictedTextboxPt.ErrorMessage = "";
+            this.restrictedTextboxPt.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.restrictedTextboxPt.Location = new System.Drawing.Point(262, 65);
             this.restrictedTextboxPt.Margin = new System.Windows.Forms.Padding(4);
             this.restrictedTextboxPt.Name = "restrictedTextboxPt";
             this.restrictedTextboxPt.Restrict = RestrictedTextbox.RestrictedTextbox.RestrictType.NoRestriction;
-            this.restrictedTextboxPt.Size = new System.Drawing.Size(227, 22);
+            this.restrictedTextboxPt.Size = new System.Drawing.Size(227, 24);
             this.restrictedTextboxPt.TabIndex = 4;
             this.restrictedTextboxPt.Text = "^([0-9]{0,3})(-[0-9]{0,4})?";
             this.restrictedTextboxPt.TrimEndChars = null;
@@ -67,11 +68,12 @@
             // 
             this.restrictedTextboxText.CapableRegexPattern = "^([0-9]{0,3})(-[0-9]{0,4})?";
             this.restrictedTextboxText.ErrorMessage = "";
-            this.restrictedTextboxText.Location = new System.Drawing.Point(36, 65);
+            this.restrictedTextboxText.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.restrictedTextboxText.Location = new System.Drawing.Point(33, 65);
             this.restrictedTextboxText.Margin = new System.Windows.Forms.Padding(4);
             this.restrictedTextboxText.Name = "restrictedTextboxText";
             this.restrictedTextboxText.Restrict = RestrictedTextbox.RestrictedTextbox.RestrictType.Custom;
-            this.restrictedTextboxText.Size = new System.Drawing.Size(218, 22);
+            this.restrictedTextboxText.Size = new System.Drawing.Size(218, 24);
             this.restrictedTextboxText.TabIndex = 3;
             this.restrictedTextboxText.TrimEndChars = null;
             // 
@@ -79,11 +81,12 @@
             // 
             this.restrictedTextboxV.CapableRegexPattern = "([0-9]+\\.?)*[0-9]*";
             this.restrictedTextboxV.ErrorMessage = "";
-            this.restrictedTextboxV.Location = new System.Drawing.Point(36, 169);
+            this.restrictedTextboxV.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.restrictedTextboxV.Location = new System.Drawing.Point(33, 169);
             this.restrictedTextboxV.Margin = new System.Windows.Forms.Padding(4);
             this.restrictedTextboxV.Name = "restrictedTextboxV";
             this.restrictedTextboxV.Restrict = RestrictedTextbox.RestrictedTextbox.RestrictType.Version;
-            this.restrictedTextboxV.Size = new System.Drawing.Size(218, 22);
+            this.restrictedTextboxV.Size = new System.Drawing.Size(218, 24);
             this.restrictedTextboxV.TabIndex = 6;
             this.restrictedTextboxV.TrimEndChars = new char[] {
         '.'};
@@ -91,11 +94,12 @@
             // comboBox1
             // 
             this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(258, 13);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 23);
+            this.comboBox1.Size = new System.Drawing.Size(173, 25);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -103,41 +107,45 @@
             // 
             this.restrictedTextbox1.CapableRegexPattern = "[0-9]";
             this.restrictedTextbox1.ErrorMessage = "";
-            this.restrictedTextbox1.Location = new System.Drawing.Point(36, 13);
+            this.restrictedTextbox1.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.restrictedTextbox1.Location = new System.Drawing.Point(33, 13);
             this.restrictedTextbox1.Margin = new System.Windows.Forms.Padding(4);
             this.restrictedTextbox1.Name = "restrictedTextbox1";
             this.restrictedTextbox1.Restrict = RestrictedTextbox.RestrictedTextbox.RestrictType.IntType;
-            this.restrictedTextbox1.Size = new System.Drawing.Size(218, 22);
+            this.restrictedTextbox1.Size = new System.Drawing.Size(218, 24);
             this.restrictedTextbox1.TabIndex = 0;
             this.restrictedTextbox1.TrimEndChars = null;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label4.Location = new System.Drawing.Point(258, 172);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(180, 15);
+            this.label4.Size = new System.Drawing.Size(230, 17);
             this.label4.TabIndex = 13;
             this.label4.Text = "Version ([0-9]+\\.?)*[0-9]*";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label2.Location = new System.Drawing.Point(258, 224);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 15);
+            this.label2.Size = new System.Drawing.Size(161, 17);
             this.label2.TabIndex = 14;
             this.label2.Text = "Custom ^[0-9]{0,4}";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(258, 117);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 15);
+            this.label1.Size = new System.Drawing.Size(197, 17);
             this.label1.TabIndex = 12;
             this.label1.Text = "AlphabetType [a-zA-z]";
             // 
@@ -145,11 +153,12 @@
             // 
             this.restrictedTextboxC.CapableRegexPattern = "^[0-9]{0,4}";
             this.restrictedTextboxC.ErrorMessage = "";
-            this.restrictedTextboxC.Location = new System.Drawing.Point(36, 221);
+            this.restrictedTextboxC.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.restrictedTextboxC.Location = new System.Drawing.Point(33, 221);
             this.restrictedTextboxC.Margin = new System.Windows.Forms.Padding(4);
             this.restrictedTextboxC.Name = "restrictedTextboxC";
             this.restrictedTextboxC.Restrict = RestrictedTextbox.RestrictedTextbox.RestrictType.Custom;
-            this.restrictedTextboxC.Size = new System.Drawing.Size(218, 22);
+            this.restrictedTextboxC.Size = new System.Drawing.Size(218, 24);
             this.restrictedTextboxC.TabIndex = 7;
             this.restrictedTextboxC.TrimEndChars = null;
             // 
@@ -157,19 +166,21 @@
             // 
             this.restrictedTextboxA.CapableRegexPattern = "[A-Za-z]";
             this.restrictedTextboxA.ErrorMessage = "";
-            this.restrictedTextboxA.Location = new System.Drawing.Point(36, 114);
+            this.restrictedTextboxA.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.restrictedTextboxA.Location = new System.Drawing.Point(33, 114);
             this.restrictedTextboxA.Margin = new System.Windows.Forms.Padding(4);
             this.restrictedTextboxA.Name = "restrictedTextboxA";
             this.restrictedTextboxA.Restrict = RestrictedTextbox.RestrictedTextbox.RestrictType.AlphabetType;
-            this.restrictedTextboxA.Size = new System.Drawing.Size(218, 22);
+            this.restrictedTextboxA.Size = new System.Drawing.Size(218, 24);
             this.restrictedTextboxA.TabIndex = 5;
             this.restrictedTextboxA.TrimEndChars = null;
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.textBox1.Location = new System.Drawing.Point(438, 14);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 22);
+            this.textBox1.Size = new System.Drawing.Size(175, 24);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -197,7 +208,7 @@
             this.Controls.Add(this.restrictedTextboxC);
             this.Controls.Add(this.restrictedTextboxA);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RestrictedTextbox Sample";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.restrictedTextboxPt)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.restrictedTextboxText)).EndInit();
